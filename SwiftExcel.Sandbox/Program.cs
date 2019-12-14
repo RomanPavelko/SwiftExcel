ce6d9280ad4fae765853874b3ee30101691518ee
Roman Pavelko <Roman.Pavelko@sovos.com>
2019-12-14 17:09:29 -0600

    Initial checkin

---
+++ SwiftExcel.Sandbox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SwiftExcel.Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
