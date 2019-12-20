0793f032576cf5f152e582b281897a9436013c72
Roman Pavelko <Roman.Pavelko@sovos.com>
2019-12-19 21:31:32 -0600

    examples update: using vs manual save

--- SwiftExcel.Sandbox/Program.cs
+++ SwiftExcel.Sandbox/Program.cs
@@ -16,29 +16,27 @@ namespace SwiftExcel.Sandbox
                         ew.Write($"row:{row}-col:{col}", col, row);
                     }
                 }
-
-                ew.Save();
             }
 
 
             //Set custom sheet name and define columns width
+            //Use manual Save() instead of using block 
             var sheet = new Sheet
             {
                 Name = "Monthly Report",
                 ColumnsWidth = new List<double> { 10, 12, 8, 8, 35 }
             };
-            using (var ew = new ExcelWriter("C:\\temp\\test.xlsx", sheet))
-            {
-                for (var row = 1; row <= 100; row++)
-                {
-                    for (var col = 1; col <= 10; col++)
-                    {
-                        ew.Write($"row:{row}-col:{col}", col, row);
-                    }
-                }
 
-                ew.Save();
+            var ew2 = new ExcelWriter("C:\\temp\\test.xlsx", sheet);
+            for (var row = 1; row <= 100; row++)
+            {
+                for (var col = 1; col <= 10; col++)
+                {
+                    ew2.Write($"row:{row}-col:{col}", col, row);
+                }
             }
+
+            ew2.Save();
         }
     }
 }
