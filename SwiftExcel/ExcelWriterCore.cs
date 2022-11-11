02801c979e8228da0615755a0688e059b3922e83
Roman Pavelko <pavelko.roma@gmail.com>
2022-11-11 00:03:21 -0800

    Replaced DateCreated and DateModified hardcoded values with real dates.

--- SwiftExcel/ExcelWriterCore.cs
+++ SwiftExcel/ExcelWriterCore.cs
@@ -129,8 +129,8 @@ namespace SwiftExcel
                          "<cp:coreProperties xmlns:cp=\"http://schemas.openxmlformats.org/package/2006/metadata/core-properties\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:dcterms=\"http://purl.org/dc/terms/\" xmlns:dcmitype=\"http://purl.org/dc/dcmitype/\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">" +
                          "<dc:creator></dc:creator>" +
                          "<cp:lastModifiedBy></cp:lastModifiedBy>" +
-                         "<dcterms:created xsi:type=\"dcterms:W3CDTF\">2015-06-05T18:17:20Z</dcterms:created>" +
-                         "<dcterms:modified xsi:type=\"dcterms:W3CDTF\">2019-11-07T23:00:46Z</dcterms:modified>" +
+                         $"<dcterms:created xsi:type=\"dcterms:W3CDTF\">{DateTime.UtcNow:yyyy-MM-ddTHH:mm:ss.FFFZ}</dcterms:created>" +
+                         $"<dcterms:modified xsi:type=\"dcterms:W3CDTF\">{DateTime.UtcNow:yyyy-MM-ddTHH:mm:ss.FFFZ}</dcterms:modified>" +
                          "</cp:coreProperties>");
             }
         }
